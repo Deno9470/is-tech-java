5e1ad304bc3a9bf0ef91dd51fb5f17cc88ced7aa
Deno9470 <Deno9470@mail.ru>
2022-02-26 08:41:52 +0300

    feat: add ClientBuilder.java

---
+++ banks-java/com/banks/Client/ClientBuilder.java
@@ -0,0 +1,41 @@
+package com.banks.Client;
+
+public class ClientBuilder {
+    private Client client = new Client();
+
+    public ClientBuilder toName(String name)
+    {
+        client.setName(name);
+        return this;
+    }
+
+    public ClientBuilder toSurname(String surname)
+    {
+        client.setSurname(surname);
+        return this;
+    }
+
+    public ClientBuilder toAddress(String address)
+    {
+        client.setAddress(address);
+        return this;
+    }
+
+    public ClientBuilder toPassportNumber(String number)
+    {
+        client.setPassportInfo(number);
+        return this;
+    }
+
+    public Client getClient()
+    {
+        Client res = client;
+        update();
+        return res;
+    }
+    private void update()
+    {
+        client = new Client();
+    }
+}
+
