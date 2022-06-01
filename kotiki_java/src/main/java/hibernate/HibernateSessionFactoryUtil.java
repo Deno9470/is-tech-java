a8c221df362fc445c3e953502ae2be815da9a33a
Deno9470 <Deno9470@mail.ru>
2022-06-01 16:41:34 +0300

    feat: hibernate cfg files

---
+++ kotiki_java/src/main/java/hibernate/HibernateSessionFactoryUtil.java
@@ -0,0 +1,30 @@
+package java.hibernate;
+import java.classes.*;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateSessionFactoryUtil {
+    private static SessionFactory _instance = null;
+
+    private HibernateSessionFactoryUtil() {}
+
+    public static SessionFactory getSessionFactory() {
+        if (_instance == null) {
+            try {
+                Configuration configuration = new Configuration().configure();
+                configuration.addAnnotatedClass(Owner.class);
+                configuration.addAnnotatedClass(Cat.class);
+                //configuration.addAnnotatedClass(Friends.class);
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties());
+                _instance = configuration.buildSessionFactory(builder.build());
+            } catch (Exception e) {
+                System.out.println("Exception: " + e.toString());
+            }
+        }
+
+        return _instance;
+    }
+}
