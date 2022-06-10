f5f4652617c8d4b3ab900730428820565a06d96d
Deno9470 <Deno9470@mail.ru>
2022-06-10 09:46:07 +0300

    feat: Cat, Owner entities

---
+++ spring_kotiki_java/src/main/java/com/entity/Owner.java
@@ -0,0 +1,48 @@
+package com.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "owners")
+public class Owner {
+    @Id
+    @SequenceGenerator(name = "sequence", sequenceName = "owners_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "sequence")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "bdate")
+    private String bdate;
+
+    public Owner(){
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Owner(String  name, String bdate) {
+        this.name = name;
+        this.bdate = bdate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return bdate;
+    }
+
+    public void setDate(String bdate) {
+        this.bdate = bdate;
+    }
+}
