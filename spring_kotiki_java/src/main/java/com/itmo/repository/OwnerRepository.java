958d2abfe0e46860ade8c19cff6b1246c8a3f822
Deno9470 <Deno9470@mail.ru>
2022-06-18 11:38:08 +0300

    feat: find by name of owners

--- spring_kotiki_java/src/main/java/com/itmo/repository/OwnerRepository.java
+++ spring_kotiki_java/src/main/java/com/itmo/repository/OwnerRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OwnerRepository extends JpaRepository<Owner, Integer> {
-
+    public Owner findFirstByName(String name);
 }
